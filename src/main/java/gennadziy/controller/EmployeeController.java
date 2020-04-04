dbad8970f22eca567068e4788671c1de953d51fd
gennadziy <37203741+gennadziy@users.noreply.github.com>
2020-04-04 20:05:27 +0200

    make it better

--- src/main/java/gennadziy/controller/EmployeeController.java
+++ src/main/java/gennadziy/controller/EmployeeController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/springboot-crud-rest/api/v1")
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
